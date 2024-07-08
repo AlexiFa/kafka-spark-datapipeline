a5060d6c8cabe87c997de8598ab1959849491d4a
Alexis Faure <113013323+AlexiFa@users.noreply.github.com>
2024-07-08 21:07:56 +0200

    add csv and push to database works

--- src/consumer.py
+++ src/consumer.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 from sqlalchemy import create_engine, text
 
-table_name = 'transactions'
+table_name = 'sanitaire'
 # Configurer le consommateur Kafka
 consumer = KafkaConsumer(
     'new',
@@ -16,10 +16,25 @@ consumer = KafkaConsumer(
 # Configurer la connexion à PostgreSQL
 engine = create_engine('postgresql://myuser:mypassword@localhost:5432/mydatabase')
 
-with engine.connect() as connection:
-    connection.execute(text('CREATE TABLE IF NOT EXISTS transactions (transaction_id TEXT, amount FLOAT, timestamp TIMESTAMP)'))
+# with engine.connect() as connection:
+#     connection.execute(text(f'CREATE TABLE transactions (transaction_id VARCHAR(50), amount INTEGER, timestamp TIMESTAMP)'))
+    # connection.execute(text(f'CREATE TABLE IF NOT EXISTS {table_name} ( \
+    #                         ligne VARCHAR(50), \
+    #                         station VARCHAR(100), \
+    #                         accessible_au_publique VARCHAR(2), \
+    #                         tarif VARCHAR(20), \
+    #                         access_pass_navigo_ou_ticket_t VARCHAR(2), \
+    #                         acces_bouton_poussoir VARCHAR(2), \
+    #                         en_zone_controlee VARCHAR(2), \
+    #                         hors_zone_controlee_station VARCHAR(2), \
+    #                         hors_zone_controlee_voie_publique VARCHAR(2), \
+    #                         accessibilite_PMR VARCHAR(2), \
+    #                         localisation TEXT, \
+    #                         coord_geo VARCHAR(100), \
+    #                         gestionnaire VARCHAR(100) \
+    #                         )'))
 #     connection.execute(text(f"DELETE FROM {table_name}"))
-
+# print(f"Table {table_name} created")
 # Traiter les messages et les écrire dans PostgreSQL
 for message in consumer:
     # Convertir le message en DataFrame Pandas
--- src/producer.py
+++ src/producer.py
@@ -1,21 +1,29 @@
 from kafka import KafkaProducer
 import json
 import time
+import csv
+
+csv_file_path = 'data/sanitaires-reseau-ratp.csv'
 
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-transactions = [
-    {"transaction_id": "3", "amount": 100, "timestamp": "2024-07-05 10:00:00"},
-    {"transaction_id": "4", "amount": 150, "timestamp": "2024-07-05 10:01:00"},
-    # Ajoutez plus de transactions ici
-]
+with open(csv_file_path, 'r') as file:
+    csv_reader = csv.DictReader(file, delimiter=';')
+    for row in csv_reader:
+        producer.send('new', value=row)
 
-for transaction in transactions:
-    producer.send('new', value=transaction)
-    time.sleep(1)
+# transactions = [
+#     {"transaction_id": "3", "amount": 100, "timestamp": "2024-07-05 10:00:00"},
+#     {"transaction_id": "4", "amount": 150, "timestamp": "2024-07-05 10:01:00"},
+#     # Ajoutez plus de transactions ici
+# ]
+
+# for transaction in transactions:
+#     producer.send('new', value=transaction)
+#     time.sleep(1)
 
 producer.flush()
 producer.close()
